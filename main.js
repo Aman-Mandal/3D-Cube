5aaee5600590eb0b43d2e671ee4c3cbec89cc714
Aman-Mandal <002amankumar@gmail.com>
2022-07-07 10:59:03 +0530

    Added camera, scene and renderer

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import * as THREE from 'three'
+
+// Scene
+const scene = new THREE.Scene()
+
+// Perspective Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+
+// WebGL Renderer
+const renderer = new THREE.WebGLRenderer()
+
+renderer.setSize(window.innerWidth, window.innerHeight)
